86cd400dcfc57e49d1aa69fcb73cce89fcc10e6a
gribanov <gribanov@protei.ru>
2020-03-05 17:43:29 +0300

    Well, this must work

--- queries.js
+++ queries.js
@@ -1,11 +1,18 @@
-const Pool = require('pg').Pool;
+/*const Pool = require('pg').Pool;
 const pool = new Pool({
     user: 'aarbrccmbkbtmp',
     host: 'ec2-18-210-51-239.compute-1.amazonaws.com',
     database: 'd7c7mngnbufq3i',
     password: '630134674690346541724fe4caa6653e24a3c84512d72489a0a1a18fadaf1c39',
     port: 5432,
+});*/
+
+const { Pool } = require('pg');
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
 });
+
 
 const getSong = (request, response) => {
     pool.query('SELECT title, record FROM ajikan.songs WHERE title_romaji LIKE "%Rewrite%"', (error, results) => {
