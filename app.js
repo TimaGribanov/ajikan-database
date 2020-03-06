084072a97fa31c41328e39c625eb5901b42cd09d
TimaGribanov <t.gribanov@yandex.ru>
2020-03-06 14:41:24 +0300

    Allowing to access API from ALL domains (later will be changed to github only)

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ app.use(
     })
 );
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // Load a html page
 app.get('/', (request, response) => {
     response.sendFile(path.join(__dirname + '/index.html'));
