0f98cdc99c9cedb0823a7b8c16ef416340225f7e
TimaGribanov <t.gribanov@yandex.ru>
2020-03-10 15:30:34 +0300

    Changed db query. Searching is working.

--- queries.js
+++ queries.js
@@ -23,7 +23,7 @@ const getRecords = (request, response) => {
 };
 
 const getSongsRecords = (request, response) => {
-    pool.query('select s.title, s.title_romaji, s.title_en r.title as record_title, case when r.type = 1 then (select title_romaji from ajikan.albums alb where r.self_id = alb.id) when r.type = 2 then (select title_romaji from ajikan.singles sin where r.self_id = sin.id) when r.type = 3 then (select title_romaji from ajikan.mini_albums malb where r.self_id = malb.id) when r.type = 4 then (select title_romaji from ajikan.compilations comp where r.self_id = comp.id) else (select title_romaji from ajikan.indie ind where r.self_id = ind.id) end as record_title_romaji from ajikan.songs s inner join ajikan.records r on r.id && s.record order by s.title', (error, results) => {
+    pool.query('select s.title, s.title_romaji, s.title_en, r.title as record_title, case when r.type = 1 then (select title_romaji from ajikan.albums alb where r.self_id = alb.id) when r.type = 2 then (select title_romaji from ajikan.singles sin where r.self_id = sin.id) when r.type = 3 then (select title_romaji from ajikan.mini_albums malb where r.self_id = malb.id) when r.type = 4 then (select title_romaji from ajikan.compilations comp where r.self_id = comp.id) else (select title_romaji from ajikan.indie ind where r.self_id = ind.id) end as record_title_romaji from ajikan.songs s inner join ajikan.records r on r.id && s.record order by s.title', (error, results) => {
         if (error) {
             throw error
         }
