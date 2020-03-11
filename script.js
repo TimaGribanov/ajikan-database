b2436ff810f52945c946004f0bae3dccf25b317b
TimaGribanov <t.gribanov@yandex.ru>
2020-03-11 14:01:13 +0300

    Version 1.0.0

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function langToEn() {
     document.getElementById('lang-ja').style.textDecorationLine = 'none';
     document.getElementById('lang-ru').style.textDecorationLine = 'none';
     document.getElementById('lang-be').style.textDecorationLine = 'none';
-    document.getElementById('info').innerHTML = 'This thing was made to get info about all recordings that an Ajikan\'s song is on quickly. I needed it for my personal reasons, but I decided that it may be useful for someone else. So, here it is, feel free to use it.\n' +
+    document.getElementById('info').innerHTML = 'I made this thing to quickly get info about all records where a certain Asian Kung-Fu Generation song can be met. I needed this for my personal use, but I think someone else may need it. So, feel free to use it.\n' +
         '    <br/>\n' +
         '    GitHub repo is <a href="https://github.com/TimaGribanov/ajikan-database" target="_blank">here</a>.';
     document.getElementById('footer').innerHTML = '<p>Timofey Gribanov © 2020</p>\n' +
@@ -23,9 +23,9 @@ function langToJa() {
     document.getElementById('lang-en').style.textDecorationLine = 'none';
     document.getElementById('lang-ru').style.textDecorationLine = 'none';
     document.getElementById('lang-be').style.textDecorationLine = 'none';
-    document.getElementById('info').innerHTML = 'This thing was made to get info about all recordings that an Ajikan\'s song is on quickly. I needed it for my personal reasons, but I decided that it may be useful for someone else. So, here it is, feel free to use it.\n' +
+    document.getElementById('info').innerHTML = '早くASIAN KUNG-FU GENERATIONの曲を含むレコードの情報を検索したいから、この事を作った。この事は個人用作っていた、でも誰かのために有用かも思わた。それでわ、<span class="line-through">また明日</span>使わてください！\n' +
         '    <br/>\n' +
-        '    GitHub repo is <a href="https://github.com/TimaGribanov/ajikan-database" target="_blank">here</a>.';
+        '    GitHubのリポジトリは<a href="https://github.com/TimaGribanov/ajikan-database" target="_blank">ここ</a>だ。';
     document.getElementById('footer').innerHTML = '<p class="ja">ティモフェイ・グリバノフ © 2020</p>\n' +
         '    <p class="ja">e-mail: <a href="mailto:t.gribanow@gmail.com">t.gribanow@gmail.com</a></p>'
 }
@@ -47,15 +47,15 @@ function langToRu() {
 
 function langToBe() {
     lang = 'be';
-    document.getElementById('label-text').innerText = 'Увядзіце назву песні';
+    document.getElementById('label-text').innerText = 'Увядзіце назву песьні';
     document.getElementById('search-button').innerText = 'Шукаць';
     document.getElementById('lang-be').style.textDecorationLine = 'underline';
     document.getElementById('lang-ja').style.textDecorationLine = 'none';
     document.getElementById('lang-ru').style.textDecorationLine = 'none';
     document.getElementById('lang-en').style.textDecorationLine = 'none';
-    document.getElementById('info').innerHTML = 'This thing was made to get info about all recordings that an Ajikan\'s song is on quickly. I needed it for my personal reasons, but I decided that it may be useful for someone else. So, here it is, feel free to use it.\n' +
+    document.getElementById('info').innerHTML = 'Гэтую рэч я зрабіў, каб хутка атрымваць інфармацыю пра ўсе запісы, на якіх сустракаеца якая-нябудзь песьня Asian Kung-Fu Generation. Яна мне была патрэбна для асабістых мэт, але я падумаў, што яна можа прыдацца камусьці яшчэ. Так што не саромейцеся карыстацца.\n' +
         '    <br/>\n' +
-        '    GitHub repo is <a href="https://github.com/TimaGribanov/ajikan-database" target="_blank">here</a>.';
+        '    Рэпазітар GitHub <a href="https://github.com/TimaGribanov/ajikan-database" target="_blank">тут</a>.';
     document.getElementById('footer').innerHTML = '<p>Цімафей Грыбанаў © 2020</p>\n' +
         '    <p>e-mail: <a href="mailto:t.gribanow@gmail.com">t.gribanow@gmail.com</a></p>'
 }
@@ -80,7 +80,7 @@ function search() {
 
     function find(inputJson) {
         alasql.promise('SELECT title, title_romaji, record_title, title_en, record_title, record_title_romaji INTO ? FROM ? WHERE title LIKE ? OR title_romaji LIKE ? OR title_en LIKE ?', [res,inputJson,searchValue,searchValue,searchValue])
-            .then(function(data) {
+            .then(function() {
                 let tempSongs = 1;
                 let tempRecords = 1;
                 if(lang === 'en' || lang === 'ru' || lang === 'be') {
