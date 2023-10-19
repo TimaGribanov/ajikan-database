e03ec5ebae3b6a4f656f5c510783d6b4bbce4602
TimaGribanov <48593815+TimaGribanov@users.noreply.github.com>
2023-10-19 16:22:50 +0200

    i18n port for prod

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -5,7 +5,7 @@ import { initReactI18next } from 'react-i18next'
 const getCurrentHost =
   import.meta.env.MODE === 'development'
     ? 'http://localhost:5173'
-    : 'http://localhost'
+    : 'http://localhost:3005'
 
 i18n
   .use(i18nBackend)
